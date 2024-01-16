12ab7f82018bd5f3ee2f6adb02a346159c87245c
KartikSGunaga <kartik.s@learner.manipal.edu>
2024-01-16 10:41:31 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.bharatintern {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.bharatintern to javafx.fxml;
+    exports org.example.bharatintern;
+}
